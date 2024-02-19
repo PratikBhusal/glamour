bb4d1e719da4a9e8063f17b676c4bc15d4ad645f
PratikBhusal <PratikBhusal@users.noreply.github.com>
2024-02-18 22:23:00 -0600

    Replace "github.com/charmbracelet/glamour" for "github.com/PratikBhusal/glamour"
    Ran the following command:
    
    ```
    rg "github.com/charmbracelet/glamour" -l -0 | xargs -0 sed -i 's/github.com\/charmbracelet\/glamour/github.com\/PratikBhusal\/glamour/g'
    ```

--- internal/generate-style-json/main.go
+++ internal/generate-style-json/main.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/charmbracelet/glamour"
-	"github.com/charmbracelet/glamour/ansi"
+	"github.com/PratikBhusal/glamour"
+	"github.com/PratikBhusal/glamour/ansi"
 )
 
 func writeStyleJSON(filename string, styleConfig *ansi.StyleConfig) error {
